1e1646b050d1610fa4d76710b6a577dcd77e2c44
GoldenEye37 <dhimbaleon356@gmail.com>
2025-08-15 15:31:55 +0200

    feat: Rates API -> design rates api endpoints - fetch all latest aggregated - fetch rates for any currency

--- app/api/rates.py
+++ app/api/rates.py
@@ -1,9 +1,36 @@
 # Rates API
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from loguru import logger
+
+from app.models import AggregatedRate, CurrencyPair
 
 rates_bp = Blueprint("rates", __name__, url_prefix="/rates")
 
 
 @rates_bp.route("/", methods=["GET"])
 def get_rates():
-    return "Here are the exchange rates"
+    try:
+        # Fetch all aggregated rates
+        rates = AggregatedRate.get_all_latest()
+        return jsonify([rate.to_dict_with_pair() for rate in rates])
+    except Exception as e:
+        logger.error(f"Error fetching rates: {e}")
+        return jsonify({"error": "Internal Server Error"}), 500
+
+
+@rates_bp.route("/rates/<string:base_or_target>", methods=["GET"])
+def get_rates_for_currency(base_or_target):
+    try:
+        error = CurrencyPair.validate_currency(base_or_target)
+        if error:
+            return jsonify({"error": error}), 400
+
+        # Fetch rates for this currency
+        rates = AggregatedRate.get_latest_for_currency(base_or_target)
+        if not rates:
+            return jsonify({}), 200
+
+        return jsonify([rate.to_dict_with_pair() for rate in rates])
+    except Exception as e:
+        logger.error(f"Error fetching rates: {e}")
+        return jsonify({"error": "Internal Server Error"}), 500
